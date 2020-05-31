a53aee54153504c323651c70831400b18408faef
Abylay Tillabek <abylay.tillabek@gmail.com>
2020-05-31 22:27:48 +0600

    fixed

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -5,8 +5,7 @@ import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
-
-fun Activity.hideKeyBoard() {
+fun Activity.hideKeyboard() {
     hideKeyBoard(currentFocus ?: View(this))
 }
 
